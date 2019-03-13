16dfdd0b46e1eab34fbf99ceadc503ea07a0c7c4
nockschii <Be9zAlksLuws8Jvrws2X>
2019-03-13 16:46:02 +0100

    lowers quality and sellIn value by one.

--- src/Item.php
+++ src/Item.php
@@ -5,16 +5,35 @@ namespace GildedRose;
 
 abstract class Item
 {
-
     private $quality;
     private $purchaseDate;
     private $expirationDate;
     private $sellIn;
 
-
-    public abstract function
+    public function __construct($quality, $purchaseDate, $expirationDate)
     {
-
+        $this->quality = $quality;
+        // TODO: resolve date problem
+        $this->expirationDate = $expirationDate;
+        $this->purchaseDate = $expirationDate;
+        $this->sellIn = $expirationDate - $purchaseDate;
     }
 
+
+    protected function lowerQualityValue()
+    {
+        $this->quality--;
+    }
+
+    protected function lowerSellInValue()
+    {
+        $this->sellIn--;
+    }
 }
+
+/*
+$rawToday = getdate();
+$today = "{rawToday['year']}-{rawToday['month']}-{rawToday['day']}";
+$date = new DateTime($today);
+echo $date->format('Y-m-d');
+*/
